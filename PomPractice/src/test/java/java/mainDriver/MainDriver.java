f6c7ecb097efd9caaa39ba68f61964f67bfe893f
andreaBuenav <andreabg1393@gmail.com>
2024-11-06 16:33:52 -0500

    Update

---
+++ PomPractice/src/test/java/java/mainDriver/MainDriver.java
@@ -0,0 +1,20 @@
+package mainDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainDriver {
+    private WebDriver driver;
+
+    public void chromeDriver( ) {
+        String path = System.getProperty("user.dir");
+       System.setProperty( "webdriver.chrome.driver", path+"util/chromedriver.exe");
+    driver = new ChromeDriver();
+    }
+
+    public WebDriver getDriver() {
+        return this.driver;
+    }
+}
+
+
