227121093195739e6640f171d8c07932ed5aafa4
andreaBuenav <andreabg1393@gmail.com>
2024-11-06 19:25:45 -0500

    Project update

--- PomPractice/src/test/java/pages/CheckoutPage.java
+++ PomPractice/src/test/java/pages/CheckoutPage.java
@@ -2,9 +2,54 @@ package pages;
 
 import basePage.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class CheckoutPage extends BasePage {
     public CheckoutPage(WebDriver driver) {
         super(driver);
     }
+
+    //Buttons
+    @FindBy(id = "shopping_cart_container")
+    private WebElement shoppingCart;
+    @FindBy(id = "checkout")
+    private WebElement checkoutButton;
+    @FindBy(id = "continue")
+    private WebElement continueButton;
+    @FindBy(id = "cancel")
+    private WebElement backButton;
+    @FindBy(id= "finish")
+    private WebElement finishButton;
+    @FindBy(id = "back-to-products")
+    private WebElement backToHomeButton;
+
+    //Inputs
+    @FindBy(id = "first-name")
+    private WebElement nameInput;
+    @FindBy(id = "last-name")
+    private WebElement lastNameInput;
+    @FindBy(id = "postal-code")
+    private WebElement zipInput;
+
+    public void checkout(){
+
+
+
+
+
+    }
+
+
+
+    public void isTitleCorrect(){
+
+    }
+
+
+
+
+
+
+
 }
--- PomPractice/src/test/java/pages/ItemPage.java
+++ PomPractice/src/test/java/pages/ItemPage.java
@@ -2,9 +2,18 @@ package pages;
 
 import basePage.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ItemPage extends BasePage {
     public ItemPage(WebDriver driver) {
         super(driver);
+    }
+    @FindBy(id = "add-to-cart")
+    private WebElement add;
+
+    public CheckoutPage selectedItems(){
+        add.click();
+      return new CheckoutPage(driver);
     }
 }
